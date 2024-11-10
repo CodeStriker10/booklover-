11d18daab3fc6d02bda9464ebe6d45ed829ab78d
Jerry Singh <khb9gd@virginia.edu>
2024-11-10 13:43:51 -0500

    Comitting booklover

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup 
+
+setup(
+    name='booklover',
+    version='0.1',
+    description='BookLover Package',
+    url='https://github.com/CodeStriker10/booklover-',
+    author='Jerry Singh',
+    author_email='scharanpreet40@gmail.com',
+    license='MIT',
+    packages=['booklover']
+)
